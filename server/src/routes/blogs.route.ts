77525a94bf69df45fd776797928b59563da39170
Salah Shaalaan <salah.shaalaan.dev@gmail.com>
2025-11-18 07:09:24 +0200

    fix mismatch in server

--- server/src/routes/blogs.route.ts
+++ server/src/routes/blogs.route.ts
@@ -1,3 +1,39 @@
+// import express from "express";
+// import {
+//   getBlogs,
+//   getBlogById,
+//   createBlog,
+//   deleteBlog,
+//   updateBlog,
+// } from "../controllers/blogs.controller";
+// import { upload } from "../middleware/multer-config";
+
+// const router = express.Router();
+
+// router.get("/", getBlogs);
+// router.post(
+//   "/",
+//   upload.fields([
+//     { name: "coverImage", maxCount: 1 },
+//     { name: "contentFile", maxCount: 1 },
+//     { name: "videoFile", maxCount: 1 }, // Added video file support
+//   ]),
+//   createBlog
+// );
+// router.get("/:id", getBlogById);
+// router.put(
+//   "/:id",
+//   upload.fields([
+//     { name: "coverImage", maxCount: 1 },
+//     { name: "contentFile", maxCount: 1 },
+//     { name: "videoFile", maxCount: 1 }, // Added video file support
+//   ]),
+//   updateBlog
+// );
+// router.delete("/:id", deleteBlog);
+
+// export default router;
+
 import express from "express";
 import {
   getBlogs,
@@ -6,30 +42,13 @@ import {
   deleteBlog,
   updateBlog,
 } from "../controllers/blogs.controller";
-import { upload } from "../middleware/multer-config";
 
 const router = express.Router();
 
 router.get("/", getBlogs);
-router.post(
-  "/",
-  upload.fields([
-    { name: "coverImage", maxCount: 1 },
-    { name: "contentFile", maxCount: 1 },
-    { name: "videoFile", maxCount: 1 }, // Added video file support
-  ]),
-  createBlog
-);
+router.post("/", createBlog);
 router.get("/:id", getBlogById);
-router.put(
-  "/:id",
-  upload.fields([
-    { name: "coverImage", maxCount: 1 },
-    { name: "contentFile", maxCount: 1 },
-    { name: "videoFile", maxCount: 1 }, // Added video file support
-  ]),
-  updateBlog
-);
+router.put("/:id", updateBlog);
 router.delete("/:id", deleteBlog);
 
 export default router;
